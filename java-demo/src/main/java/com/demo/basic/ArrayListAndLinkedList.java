577d8f3b2f856fa516cc7692034dd82cf1a65a09
zouyongsheng <zouyongsheng>
2019-11-30 13:15:00 +0800

    code commit

--- java-demo/src/main/java/com/demo/basic/ArrayListAndLinkedList.java
+++ java-demo/src/main/java/com/demo/basic/ArrayListAndLinkedList.java
@@ -5,9 +5,14 @@ import java.util.Iterator;
 import java.util.LinkedList;
 
 /**
- * Description: arrayList for循环效率高，linedlist Iterator效率高
+ * Description: arrayList for循环效率高，linedlist steam效率高
  * ArrayList 对于随机位置的add/remove，时间复杂度为 O(n),但是对于列表末尾的添加/删除操作,时间复杂度是 O(1).
  * LinkedList对于随机位置的add/remove，时间复杂度为 O(n),但是对于列表 末尾/开头 的添加/删除操作,时间复杂度是 O(1).
+ *
+ * 对于简单操作，比如最简单的遍历，Stream串行API性能明显差于显示迭代，但并行的Stream API能够发挥多核特性。
+ * 对于复杂操作，Stream串行API性能可以和手动实现的效果匹敌，在并行执行时Stream API效果远超手动实现。
+ * 所以，如果出于性能考虑，1. 对于简单操作推荐使用外部迭代手动实现，2. 对于复杂操作，推荐使用Stream API，
+ * 3. 在多核情况下，推荐使用并行Stream API来发挥多核优势，4.单核情况下不建议使用并行Stream API。
  * <p>
  * Create on 2019/11/30
  *
@@ -94,6 +99,19 @@ public class ArrayListAndLinkedList {
         }
         System.out.println(System.nanoTime() - start +" <--ArrayList for");
 
+        start = System.nanoTime();
+        arrayList.stream().forEach(list ->{
+            int i = list;
+        });
+        System.out.println(System.nanoTime() - start +" <--ArrayList 串行steam");
+
+        start = System.nanoTime();
+        arrayList.stream().parallel().forEach(list ->{
+            int i = list;
+        });
+        System.out.println(System.nanoTime() - start +" <--ArrayList 并行steam");
+
+
         System.out.println("-------------------------");
         start = System.nanoTime();
         for (Integer s : linkedList){
@@ -116,6 +134,12 @@ public class ArrayListAndLinkedList {
 
         }
         System.out.println(System.nanoTime() - start +" <--linkedList for");
+
+        start = System.nanoTime();
+        linkedList.stream().forEach(list ->{
+            int i = list;
+        });
+        System.out.println(System.nanoTime() - start +" <--linkedList steam");
         System.out.println("-------------------------");
 
         // ArrayList remove
@@ -141,22 +165,24 @@ public class ArrayListAndLinkedList {
 //          输出结果
 //        Test times = 100000
 //                -------------------------
-//                8848700 <--ArrayList add
-//        17676700 <--LinkedList add
+//                11986600 <--ArrayList add
+//        25752100 <--LinkedList add
 //                -------------------------
-//                7474700 <--ArrayList get
-//        4582492100 <--LinkedList get
+//                856300 <--ArrayList get
+//        3751935200 <--LinkedList get
 //                ---------------------循环对比
-//        2788000 <--ArrayList foreach
-//        47490700 <--ArrayList Iterator
-//        1098600 <--ArrayList for
-//        -------------------------
-//                4365500 <--linkedList foreach
-//        10721400 <--linkedList Iterator
-//        3775830200 <--linkedList for
-//        -------------------------
-//                4442300 <--ArrayList remove
-//        8909600 <--LinkedList remove
+//        3291400 <--ArrayList foreach
+//        40334500 <--ArrayList Iterator
+//        4499900 <--ArrayList for
+//        48762100 <--ArrayList steam
+//                -------------------------
+//                4900200 <--linkedList foreach
+//        14752000 <--linkedList Iterator
+//        3598482700 <--linkedList for
+//        4143200 <--linkedList steam
+//                -------------------------
+//                4266100 <--ArrayList remove
+//        9590800 <--LinkedList remove
 
 
     }
