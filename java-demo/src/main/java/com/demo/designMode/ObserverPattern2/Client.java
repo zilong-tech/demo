65c16e3b200f2c7783b343ac86b2547c746b0c4e
zys <zysspace@163.com>
2021-12-01 23:18:18 +0800

    中介者模式

--- java-demo/src/main/java/com/demo/designMode/ObserverPattern2/Client.java
+++ java-demo/src/main/java/com/demo/designMode/ObserverPattern2/Client.java
@@ -10,15 +10,16 @@ public class Client {
     public static void main(String[] args) {
 
         ConcreteSubject subject = new ConcreteSubject();
-        // 这里假设是添加好友
+        // 这里假设是增加新用户
         subject.attach(new ConcreteObserver());
         ConcreteObserver twoObserver = new ConcreteObserver();
         subject.attach(twoObserver);
 
         // 发送朋友圈动态
-        subject.notifyObservers("第一个朋友圈消息");
+        subject.notifyObservers("第一条消息");
 
         subject.detach(twoObserver);
-        subject.notifyObservers("第二个朋友圈消息");
+
+        subject.notifyObservers("第二个消息");
     }
 }
--- java-demo/src/main/java/com/demo/designMode/ObserverPattern2/ConcreteObserver.java
+++ java-demo/src/main/java/com/demo/designMode/ObserverPattern2/ConcreteObserver.java
@@ -10,6 +10,6 @@ public class ConcreteObserver implements Observer{
     @Override
     public void update(String message) {
         // 模拟处理业务逻辑
-        System.out.println("FriendOne 知道了你发动态了" + message);
+        System.out.println("用户收到消息：" + message);
     }
 }
